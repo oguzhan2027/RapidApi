4cb9095b72b1ba58108d3b26300a4e582a133198
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-20 14:13:48 +0300

    ...

--- Casgem_RapidApi/Controllers/ExchangeController.cs
+++ Casgem_RapidApi/Controllers/ExchangeController.cs
@@ -13,7 +13,7 @@ namespace Casgem_RapidApi.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=USD&locale=en-gb"),
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
                 Headers =
     {
         { "X-RapidAPI-Key", "2d7c2f32c8msh15391ec0d5ae2bep13c8fcjsnf59db3069ad3" },
@@ -30,5 +30,30 @@ namespace Casgem_RapidApi.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> Index2(string x = "TRY")
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency={x}&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "2d7c2f32c8msh15391ec0d5ae2bep13c8fcjsnf59db3069ad3" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+
+            }
+            return View();
+        }
+
     }
 }
